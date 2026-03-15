59fda8e2f17436b513ccef7876b354fff031e981
JHay <judehayespro@gmail.com>
2026-03-15 13:14:37 -0700

    Removing setup script

--- scripts/setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-git submodule update --init --recursive
-
-vcpkg install --triplet x64-windows-static-md-ffmpeg-dynamic --overlay-triplets=triplets
-
-:: Root build with CommonLibF4 flags
-cmake --preset windows-x64
-cmake --build --preset build-release
